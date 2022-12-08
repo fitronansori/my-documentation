9ecd71d4b55ee7a3d7fc88491410737bfbde9343
Fitron Ansori <fitronansori7@gmail.com>
2022-12-08 10:50:01 +0800

    belajar useCallback dan useMemo

--- ReactHooks/example/react-hooks/src/App.jsx
+++ ReactHooks/example/react-hooks/src/App.jsx
@@ -1,34 +1,61 @@
-import React, { useState, useReducer } from "react";
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
-import { todoReducer, initialState } from "./context/reducer/todosReducer";
-
-import { Provider, todos } from "./context/store/store";
+import { useState, useCallback } from "react";
+import Todos from "./components/Todos";
 
 const App = () => {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
-  const [todo, setTodoText] = useState("");
-  console.log(state);
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch({ type: "ADD_TODO", payload: todo });
-    setTodoText("");
+  const increment = () => {
+    setCount((c) => c + 1);
   };
+  const addTodo = useCallback(() => {
+    setTodos((t) => [...t, "New Todo"]);
+  }, [todos]);
 
   return (
-    <Provider value={todos}>
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
       <div>
-        <h1>Todo App</h1>
-        <TodoInput
-          handleSubmit={handleSubmit}
-          todos={todo}
-          setTodoText={setTodoText}
-        />
-        <TodoList todos={state.todos} />
+        Count: {count}
+        <button onClick={increment}>+</button>
       </div>
-    </Provider>
+    </>
   );
 };
 
 export default App;
+// import React, { useState, useReducer } from "react";
+// import TodoInput from "./components/TodoInput";
+// import TodoList from "./components/TodoList";
+// import { todoReducer, initialState } from "./context/reducer/todosReducer";
+
+// import { Provider, todos } from "./context/store/store";
+
+// const App = () => {
+//   const [state, dispatch] = useReducer(todoReducer, initialState);
+//   const [todo, setTodoText] = useState("");
+//   console.log(state);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     dispatch({ type: "ADD_TODO", payload: todo });
+//     setTodoText("");
+//   };
+
+//   return (
+//     <Provider value={todos}>
+//       <div>
+//         <h1>Todo App</h1>
+//         <TodoInput
+//           handleSubmit={handleSubmit}
+//           todos={todo}
+//           setTodoText={setTodoText}
+//         />
+//         <TodoList todos={state.todos} />
+//       </div>
+//     </Provider>
+//   );
+// };
+
+// export default App;
