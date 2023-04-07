36baed4bddc710b666cb825d87ee22b0eb449bad
Fitron Ansori <fitronansori7@gmail.com>
2023-04-07 15:36:13 +0800

    belajar python

---
+++ Python/Code/ControlFlow.py
@@ -0,0 +1,73 @@
+# If Statement
+
+# nama = input("Masukkan nama anda: ")
+# age = input("Masukkan umur anda: ")
+
+# if int(age) >= 18:
+#     print(f"{nama} anda sudah boleh mengikuti pemilu.")
+
+# If Else Statement
+
+# nama = input("Masukkan nama anda: ")
+# age = input("Masukkan umur anda: ")
+
+# if int(age) >= 18:
+#     print(f"{nama} anda sudah boleh mengikuti pemilu.")
+# else:
+#     print(f"{nama} anda belum boleh mengikuti pemilu.")
+
+# If Elif Else Statement
+
+# age = input('Enter your age:')
+
+# # convert the string to int
+# your_age = int(age)
+
+# # determine the ticket price
+# if your_age < 5:
+#     ticket_price = 5
+# elif your_age < 16:
+#     ticket_price = 10
+# else:
+#     ticket_price = 18
+
+# # show the ticket price
+# print(f"You'll pay ${ticket_price} for the ticket")
+
+
+# Looping
+
+# Range
+
+# for i in range(10):
+#     print(i)
+    
+# Custom Range
+
+# for i in range(10, 20, 2):
+#     print(i)
+
+
+# # for loop with list
+# for i in [1, 2, 3, 4, 5]:
+#     print(i)
+
+# # for loop with tuple
+# for i in (1, 2, 3, 4, 5):
+#     print(i)
+
+# # for loop with set
+# for i in {1, 2, 3, 4, 5}:
+#     print(i)
+
+# # for loop with dictionary
+# for i in {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}:
+#     print(i)
+
+# # for loop with string
+# for i in 'Hello World':
+#     print(i)
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(f'{i} x {j} = {i * j}')
