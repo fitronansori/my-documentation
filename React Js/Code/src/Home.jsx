30c3982cf50939685dc4d3bc2453b2777c48f2c0
Fitron Ansori <fitronansori7@gmail.com>
2023-04-28 13:19:47 +0800

    react

---
+++ React Js/Code/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hello, My name is Fitron Ansori</h1>
+      <span>Front-End Developer</span>
+    </>
+  );
+};
+
+export default Home;
